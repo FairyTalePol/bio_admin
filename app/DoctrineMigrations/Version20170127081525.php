5496d389f46c462653687bfd7fcd4bbaa2898efc
Dima S <dimka@3g.ua>
2017-01-27 14:53:58 +0200

    fix migration

--- app/DoctrineMigrations/Version20170127081525.php
+++ app/DoctrineMigrations/Version20170127081525.php
@@ -16,10 +16,10 @@ class Version20170127081525 extends AbstractMigration
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('GRANT SELECT ON TABLE public.vermin_categories TO bio_group;');
-        $this->addSql('GRANT SELECT ON TABLE public.vermins TO bio_group;');
-        $this->addSql('GRANT SELECT ON SEQUENCE public.vermin_categories_id_seq TO bio_group;');
-        $this->addSql('GRANT SELECT ON SEQUENCE public.vermins_id_seq TO bio_group;');
+        $this->addSql('GRANT SELECT ON TABLE public.vermin_categories TO bio_group, farming_group;');
+        $this->addSql('GRANT SELECT ON TABLE public.vermins TO bio_group, farming_group;');
+        $this->addSql('GRANT SELECT ON SEQUENCE public.vermin_categories_id_seq TO bio_group, farming_group;');
+        $this->addSql('GRANT SELECT ON SEQUENCE public.vermins_id_seq TO bio_group, farming_group;');
 
     }
 
@@ -29,10 +29,10 @@ class Version20170127081525 extends AbstractMigration
     public function down(Schema $schema)
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('REVOKE SELECT ON TABLE public.vermin_categories FROM bio_group;');
-        $this->addSql('REVOKE SELECT ON TABLE public.vermins FROM bio_group;');
-        $this->addSql('REVOKE SELECT ON SEQUENCE public.vermin_categories_id_seq FROM bio_group;');
-        $this->addSql('REVOKE SELECT ON SEQUENCE public.vermins_id_seq FROM bio_group;');
+        $this->addSql('REVOKE SELECT ON TABLE public.vermin_categories FROM bio_group, farming_group;');
+        $this->addSql('REVOKE SELECT ON TABLE public.vermins FROM bio_group, farming_group;');
+        $this->addSql('REVOKE SELECT ON SEQUENCE public.vermin_categories_id_seq FROM bio_group, farming_group;');
+        $this->addSql('REVOKE SELECT ON SEQUENCE public.vermins_id_seq FROM bio_group, farming_group;');
 
     }
 }
